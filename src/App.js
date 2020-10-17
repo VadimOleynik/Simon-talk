7bcda8667dc5063df40846522a41a0e199f508be
Vadim Oleynik <oleynik_vs@citrus.com.ua>
2020-10-17 11:53:29 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './store/reducers'
+
+import Field from "./components/Field";
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <Field/>
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
